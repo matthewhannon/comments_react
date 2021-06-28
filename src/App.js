bb8d6d8a69cbafa8d34658ae9bd09014a5d704ab
Matt Hannon <matthannon88@gmail.com>
2021-06-27 22:56:01 -0700

    Initial Project Version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import faker from 'faker';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.addComment = this.addComment.bind(this);
+        this.state = {
+            comments: []
+        }
+    }
+
+    addComment(e) {
+        e.preventDefault();
+
+        let comment = e.target.elements.comment.value;
+
+        this.setState((prevState) => {
+            return {
+                comments: prevState.comments.concat(comment)
+            }
+        })
+
+        e.target.elements.comment.value = '';
+    }
+
+    render(){
+        return (
+            <div className="title-container">
+                <h2 className="title">Comment!</h2>
+                {this.state.comments.length > 0 && <CommentCard comments={this.state.comments}/>}
+                <AddComment addComment={this.addComment}/>
+            </div>
+        )
+    }
+}
+
+const AddComment = (props) => {
+    return (
+        <div>
+            <form onSubmit={props.addComment}>
+                <input type="text" name="comment"></input>
+                <button>POST</button>
+            </form>
+        </div>
+    )
+}
+
+
+const CommentCard = (props) => {
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes();
+    var dateTime = date+' '+time;
+    
+    return (
+        props.comments.map((comment, index) => {
+            return (
+                <div className="card-container" key={index}>
+                    <div className="left-card">
+                        <img src={faker.image.avatar()}></img>
+                        <h3>{faker.name.firstName()}</h3>
+                        <p>{dateTime}</p>
+                    </div>
+                    <div className="right-card">
+                        <p>{comment}</p>
+                    </div>
+                </div>
+            )
+        })
+    )
 }
 
 export default App;
